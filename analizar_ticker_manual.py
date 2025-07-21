742939bedd31ef7c9b978abdb54c4b7250770894
ibexia <xumkox@gmail.com>
2025-07-21 09:54:25 +0200

    Update analizar_ticker_manual.py

--- analizar_ticker_manual.py
+++ analizar_ticker_manual.py
@@ -1089,7 +1089,7 @@ def generar_contenido_con_gemini(tickers):
 
 def main():
     # Define el ticker que quieres analizar
-    ticker_deseado = "ADX.MC"  # <-- ¡CAMBIA "BBVA.MC" por el Ticker que quieras analizar!
+    ticker_deseado = "REP.MC"  # <-- ¡CAMBIA "BBVA.MC" por el Ticker que quieras analizar!
                                 # Por ejemplo: "REP.MC", "TSLA", etc.
 
     # Prepara la lista de tickers para la función generar_contenido_con_gemini
