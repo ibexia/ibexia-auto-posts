ee57381320a4ae3a89c1e0527a1d2a0050049ffd
ibexia <xumkox@gmail.com>
2025-07-24 12:59:07 +0200

    Update analizar_ticker_manual.py

--- analizar_ticker_manual.py
+++ analizar_ticker_manual.py
@@ -1121,7 +1121,7 @@ def generar_contenido_con_gemini(tickers):
 
 def main():
     # Define el ticker que quieres analizar
-    ticker_deseado = "IBG.MC"
+    ticker_deseado = "IBE.MC"
 
     tickers_for_today = [ticker_deseado]
 
