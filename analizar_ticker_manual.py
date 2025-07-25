522c35173e8733d638d511c907c933af87a60cf3
ibexia <xumkox@gmail.com>
2025-07-25 17:33:43 +0200

    Update analizar_ticker_manual.py

--- analizar_ticker_manual.py
+++ analizar_ticker_manual.py
@@ -1160,7 +1160,7 @@ def generar_contenido_con_gemini(tickers):
 
 def main():
     # Define el ticker que quieres analizar
-    ticker_deseado = "CITY.MC"
+    ticker_deseado = "END.MC"
 
     tickers_for_today = [ticker_deseado]
 
